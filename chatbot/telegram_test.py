32c711a051ec4a98ae32c9d5591469bb0de5f194
rjvervelde@hetnet.nl <rjvervelde@hetnet.nl>
2020-03-28 13:20:55 +0100

    skl

--- chatbot/telegram_test.py
+++ chatbot/telegram_test.py
@@ -1,36 +1,27 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
-import aiml
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()    
     url = contents['url']
     return url
 
+def start_callback(update, context):
+    user_says = " ".join(context.args)
+    update.message.reply_text("You said: " + user_says)
+    update.message.reply_text("Welcome to my awesome bot!")
+    '''chat_id = update.message.chat_id
+    bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my awesome bot!")'''
+
 def talk(bot, update):
-    while True:
-        message = input("Enter your message >> ")
-        
-        if message == "quit":
-            break #useful in notebook
-            # exit() # useful in actual application
-        elif message == "save":
-            kernel.saveBrain("bot_brain.brn")
-        else:
-            bot_response = kernel.respond(message)
-            # star, user_response = extract_information()
-            #process_response = process_user_response(user_response, star)
-            # Do something with bot_response
-            # syntactic parse
-            # prosodic parse
-            
-            #print(bot_response)
-    return bot_response
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 def main():
     updater = Updater('999799466:AAEt6tJbek9P5qC1ZlHTrKmDmU34dpmjDB8')
     dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start_callback))
     dp.add_handler(CommandHandler('talk',talk))
     updater.start_polling()
     updater.idle()
