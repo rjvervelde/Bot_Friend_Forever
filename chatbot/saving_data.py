0f7bf53ee466cf5e50d935ebd5c190e9bfcbb92e
EllenBogaards <ellenbogaards@gmail.com>
2020-03-29 13:08:12 +0200

    Update_saving_data.py
    Nu ook iets werkend voor gender, maar heb hier geen waterdichte constructie voor

--- chatbot/saving_data.py
+++ chatbot/saving_data.py
@@ -4,7 +4,7 @@ from nltk.corpus import stopwords
 stop = stopwords.words('english')
 
 # Example of user input
-string = """ Hey! My name is Ellen. I am 22 years old. 
+string = """ Hey! My name is Ellen. I am 22 years old. I am a woman.
 You can reach met at ellenbogaards@gmail.com, please send me an email if you want to contact me!"""
 
 # string = input("Enter your message >> ")
@@ -17,6 +17,18 @@ def extract_age(string):
 def extract_email_addresses(string):
     r = re.compile(r'[\w\.-]+@[\w\.-]+')
     return r.findall(string)
+
+def extract_gender(string):
+    f = ['female', 'woman', 'girl', 'lady', 'mrs', 'ms']
+    m = ['male', 'man', 'boy', 'sir', 'mr']
+    gender = 'unkown'
+    for i in m:
+        if i in string:
+            gender = 'male'
+    for i in f:
+        if i in string:
+            gender = 'female'
+    return gender
 
 def ie_preprocess(document):
     document = ' '.join([i for i in document.split() if i not in stop])
@@ -35,10 +47,13 @@ def extract_names(document):
                     names.append(' '.join([c[0] for c in chunk]))
     return names
 
+
 if __name__ == '__main__':
     age = extract_age(string)
     emails = extract_email_addresses(string)
     names = extract_names(string)
+    gender = extract_gender(string)
     print("The name of the user is", names[0])
     print("The user is",age[0],"years old")
     print("The e-mailadres of the user is", emails[0])
+    print("The gender of the user is", gender)
