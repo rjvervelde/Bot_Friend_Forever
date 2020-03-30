ed3b8ead65952d16ec57df75b0bc3794db61743a
sabijn <sabijn.perdijjk@gmail.com>
2020-03-30 17:35:37 +0200

    lalala

--- chatbot/lab4_sabijn.py
+++ chatbot/lab4_sabijn.py
@@ -1,11 +1,12 @@
 import aiml
 from sentiment_fancy import sentiment_of_text
-from datetime import date
+from datetime import date, datetime
 
 def set_parameters():
     kernel.setPredicate("status", "neutral")
-    kernel.setPredicate("birthday", "infinity!!!!")
+    kernel.setPredicate("birthday", "everyday there is time for a party")
     kernel.setPredicate("age", "undefined")
+    kernel.setPredicate("age_category", "undefined")
     kernel.setPredicate("how_many_days", "it is always youre birthday")
     return
 
@@ -14,6 +15,7 @@ def birthday(born):
     d = today.strftime("%d/%m/%Y")
     current_day, current_month, current_year = d.split("/")
     birth_day, birth_month, birth_year = born.split()[-1].split('-')
+    kernel.setPredicate("birthday", f"{born.split()[-1]}")
     
     if int(birth_month) < int(current_month):
         diff = int(current_year) - int(birth_year)
@@ -32,9 +34,20 @@ def birthday(born):
 
     return f"{diff}", classs
 
-# def calculate_until_birthday(user_message)
+def calculate_until_birthday():
+    birthday = kernel.getPredicate("birthday")
+    birth_day, birth_month, birth_year = birthday.split('-')
+
+    today = datetime.now()
+
+    delta1 = datetime(today.year, int(birth_month), int(birth_day))
+    delta2 = datetime(today.year+1, int(birth_month), int(birth_day))
+    days = (max(delta1, delta2) - today).days
+
+    return f"{days}"
 
 def extract_information(user_message):
+    # sentiment analysis
     if kernel.getPredicate("status"):
         sentiment = sentiment_of_text(user_message)
         if sentiment < 0:
@@ -43,15 +56,16 @@ def extract_information(user_message):
         else:
             kernel.setPredicate("status", "good")
     
+    # calculate age
     for element in user_message:
         if element == '-':
-            kernel.setPredicate("birthday", birthday(user_message)[0])
-            kernel.setPredicate("age", birthday(user_message)[1])
+            kernel.setPredicate("age", birthday(user_message)[0])
+            kernel.setPredicate("age_category", birthday(user_message)[1])
             break
     
-    # if user_message.lower() == "how long till it is my birthday":
-    #     calculate_until_birthday(user_message)
-    #     kernel.setPredicate()
+    # how many days until you're birthday
+    if user_message.lower()[-8:] == "birthday":
+        kernel.setPredicate("how_many_days", calculate_until_birthday())
 
     return
 
